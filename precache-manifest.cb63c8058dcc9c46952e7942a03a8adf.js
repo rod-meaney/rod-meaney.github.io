7891d41cd48e5926c013427448f79c45eb18bac0
rodm <rod.meaney@gmail.com>
2020-03-05 21:08:02 +1100

    fun 2

---
+++ precache-manifest.cb63c8058dcc9c46952e7942a03a8adf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362f59c57b75fc5663003dcb5fafdb1a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "49d2366220cd424ad1ca",
+    "url": "./static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "2fa7b399b23123825a95",
+    "url": "./static/css/main.aec124fd.chunk.css"
+  },
+  {
+    "revision": "49d2366220cd424ad1ca",
+    "url": "./static/js/2.dc7a088a.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "./static/js/2.dc7a088a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa7b399b23123825a95",
+    "url": "./static/js/main.f4bda597.chunk.js"
+  },
+  {
+    "revision": "033e3c6f790567ea4b44",
+    "url": "./static/js/runtime-main.1878c982.js"
+  },
+  {
+    "revision": "be7e024f8baa592685318792f8e10b99",
+    "url": "./static/media/home.be7e024f.jpg"
+  }
+]);
