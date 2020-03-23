476a4edc0ccbb472a01da2865f4d04707284a666
rodm <rod.meaney@gmail.com>
2020-03-23 11:41:31 +1100

    Testing orientation screen

---
+++ precache-manifest.e26d58cdccd15399b1444f93e6955dea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e504b71fd54a9a0f93c19b3905cb4b8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "961ba5efa09c75502128",
+    "url": "./static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "124b309abe0c4ab79150",
+    "url": "./static/css/main.aec124fd.chunk.css"
+  },
+  {
+    "revision": "961ba5efa09c75502128",
+    "url": "./static/js/2.a611f92e.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "./static/js/2.a611f92e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "124b309abe0c4ab79150",
+    "url": "./static/js/main.c90aff50.chunk.js"
+  },
+  {
+    "revision": "033e3c6f790567ea4b44",
+    "url": "./static/js/runtime-main.1878c982.js"
+  },
+  {
+    "revision": "ad458e0ddad1f67c6afa30c4cdbc6c49",
+    "url": "./static/media/correct.ad458e0d.wav"
+  },
+  {
+    "revision": "67c7dbb8f097093766798ba3c8a949aa",
+    "url": "./static/media/incorrect.67c7dbb8.wav"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.877ec4d08f4e2d5c4530d670c0631c84.js"
+  "./precache-manifest.e26d58cdccd15399b1444f93e6955dea.js"
 );
 
 self.addEventListener('message', (event) => {
