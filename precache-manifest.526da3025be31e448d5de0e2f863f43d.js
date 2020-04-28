09dfcc44a5e803c28e69ae34a17d07852cba9384
rodm <rod.meaney@gmail.com>
2020-04-28 20:16:51 +1000

    Simple site

---
+++ precache-manifest.526da3025be31e448d5de0e2f863f43d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32bdc2c9c0a266d82f7e39fe71d8b4e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc8a2d475374865c6604",
+    "url": "/static/css/2.931d0167.chunk.css"
+  },
+  {
+    "revision": "df5acf8a7fef81aac9f4",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "fc8a2d475374865c6604",
+    "url": "/static/js/2.5d4d7f23.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5d4d7f23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df5acf8a7fef81aac9f4",
+    "url": "/static/js/main.aa74acf4.chunk.js"
+  },
+  {
+    "revision": "f20dbce13e4a3f817e26",
+    "url": "/static/js/runtime-main.a5bb09cc.js"
+  }
+]);
