3800d21792075b618f3dbd0fa2a59e33c7afdd37
rodm <rod.meaney@gmail.com>
2020-08-21 12:18:41 +1000

    update subsite config

---
+++ tremor/precache-manifest.a5ad16991c7803c6713011808eeeaed5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f7fe8df112feaf27ef0a926c000554",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac7b3c96d2b4d871835c",
+    "url": "./static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "cb96e4c30d5f27928fde",
+    "url": "./static/css/main.c0710856.chunk.css"
+  },
+  {
+    "revision": "ac7b3c96d2b4d871835c",
+    "url": "./static/js/2.865856bd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.865856bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb96e4c30d5f27928fde",
+    "url": "./static/js/main.01fe465f.chunk.js"
+  },
+  {
+    "revision": "4ea42e7b64ed2e08ae3a",
+    "url": "./static/js/runtime-main.0a1aebd6.js"
+  }
+]);
--- tremor/service-worker.js
+++ tremor/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0eb6c7719c2e2e075efeef88f20bbecb.js"
+  "./precache-manifest.a5ad16991c7803c6713011808eeeaed5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
